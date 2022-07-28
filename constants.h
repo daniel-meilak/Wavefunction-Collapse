405444fa227a35e149656f6265f7355ef7c7d8e4
daniel.mlak@gmail.com <daniel.mlak@gmail.com>
2022-07-28 23:06:23 +0200

    added comments and conveniant variables to constants.h

--- constants.h
+++ constants.h
@@ -1,4 +1,32 @@
 #pragma once
 
-constexpr int screenWidth{1200};
-constexpr int screenHeight{100};
+#include<array>
+#include<cstddef>
+#include<utility>
+
+#include"point.h"
+
+// tile size multiplier
+constexpr int scale{3};
+
+// tile width/height original size (pixels)
+constexpr int tileSize{14};
+constexpr int tileScaled{scale*tileSize};
+
+// grid dimensions
+constexpr int gridWidth{10};
+constexpr int gridHeight{10};
+
+// window size
+constexpr int screenWidth{gridWidth*tileSize*scale};
+constexpr int screenHeight{gridHeight*tileSize*scale};
+
+// bitset max and alias
+constexpr std::size_t N=64;
+using Bitset = std::bitset<N>;
+
+// cardinal directions (right, bottom, left, up)
+constexpr std::array<Point,4> cardinals{Point(1,0),Point(0,-1),Point(-1,0),Point(0,1)};
+
+// clearer names for rotate() 
+enum dir{clockwise=true, anticlockwise=false};
