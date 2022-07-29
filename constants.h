2615609d32891481c20077d5672675bbeac0d210
daniel.mlak@gmail.com <daniel.mlak@gmail.com>
2022-07-29 19:40:15 +0200

    fixed cardinals array

--- constants.h
+++ constants.h
@@ -14,8 +14,8 @@ constexpr int tileSize{14};
 constexpr int tileScaled{scale*tileSize};
 
 // grid dimensions
-constexpr int gridWidth{10};
-constexpr int gridHeight{10};
+constexpr int gridWidth{20};
+constexpr int gridHeight{15};
 
 // window size
 constexpr int screenWidth{gridWidth*tileSize*scale};
@@ -26,7 +26,7 @@ constexpr std::size_t N=64;
 using Bitset = std::bitset<N>;
 
 // cardinal directions (right, bottom, left, up)
-constexpr std::array<Point,4> cardinals{Point(1,0),Point(0,-1),Point(-1,0),Point(0,1)};
+constexpr std::array<Point,4> cardinals{Point(1,0),Point(0,1),Point(-1,0),Point(0,-1)};
 
 // clearer names for rotate() 
 enum dir{clockwise=true, anticlockwise=false};
