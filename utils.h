67b9805c9c6f389dd4251649f30c8bb4a325ae21
daniel.mlak@gmail.com <daniel.mlak@gmail.com>
2022-08-12 19:36:00 +0200

    added utils header for printing full paths

---
+++ utils.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<string>
+
+#include"globals.h"
+
+// get full path to tilesetFile
+std::string pathToTexture(const std::string& tilesetDir){
+   std::string result;
+   result.append(tilesetBaseDir);
+   result += '/' + tilesetDir + '/';
+   result.append(tilesetFile);
+
+   return result; 
+}
+
+// get full path to tilesetData
+std::string pathToData(const std::string& tilesetDir){
+   std::string result;
+   result.append(tilesetBaseDir);
+   result += '/' + tilesetDir + '/';
+   result.append(tilesetDataFile);
+
+   return result; 
+}
