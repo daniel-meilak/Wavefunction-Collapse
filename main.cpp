5962e314e4a5fa98b31b929c0cf2d265200286fa
daniel.mlak@gmail.com <daniel.mlak@gmail.com>
2022-08-15 23:43:10 +0200

    changed main for compilation using emcc

--- main.cpp
+++ main.cpp
@@ -9,43 +9,40 @@
 #include"menu.h"
 #include"storage.h"
 
+#ifdef PLATFORM_WEB
+    #include<emscripten/emscripten.h>
+#endif
+
+// set up global pointers
+Grid* gridPtr;
+MenuControl* menusPtr;
+
+void UpdateDrawFrame();
+
+
+
 int main(){
 
-    InitWindow(screenWidth, screenHeight, "Wavefunction Collapse"); 
+    InitWindow(screenWidth, screenHeight, "Wavefunction Collapse");
 
     // create grid with tileset and data sheet
     Grid grid(tilesetDir);
+    gridPtr = &grid;
 
     // create controlls and tile select menu
     MenuControl menus(
-        createControlsMenu(screenWidth-700.0f, screenHeight-105.0f, grid, Grid::reset, grid.running, grid.updateSpeed),
+        createControlsMenu(screenWidth-700.0f, screenHeight-105.0f, grid, &Grid::reset, grid.running, grid.updateSpeed),
         createTilesMenu(10.0f, 10.0f, tilesetDir, weights, grid)
     );
+    menusPtr = &menus;
 
-    SetTargetFPS(fps);
-    while(!WindowShouldClose()){
-
-        // add elapsed time since last update
-        sinceLastUpdate += GetFrameTime();
-
-        // update mousepos
-        mousePos = GetMousePosition();
-
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        // reset on completion
-        if (grid.collapsed){ grid.reset(); }
-
-        // draw grid and menu
-        grid.draw();
-        menus.display();
-
-        // collapse next grid position
-        grid.update();
-
-        EndDrawing();
-    }
+    // Web version uses emscripten function
+    #ifdef PLATFORM_WEB
+        emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+    #else
+        SetTargetFPS(fps);
+        while(!WindowShouldClose()){ UpdateDrawFrame(); }
+    #endif
 
     // unload all fonts and textures
     textureStore.unloadAll();
@@ -54,4 +51,29 @@ int main(){
     CloseWindow();
 
     return 0;
+}
+
+// main loop funciton
+void UpdateDrawFrame(){
+
+    // add elapsed time since last update
+    sinceLastUpdate += GetFrameTime();
+
+    // update mousepos
+    mousePos = GetMousePosition();
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    // reset on completion
+    if (gridPtr->collapsed){ gridPtr->reset(); }
+
+    // draw grid and menu
+    gridPtr->draw();
+    menusPtr->display();
+
+    // collapse next grid position
+    gridPtr->update();
+
+    EndDrawing();
 }
